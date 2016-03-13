331dc4c66186a82b1d6cf4ec1eb40a3e34cdc573
Shea Levy <shea@shealevy.com>
2016-03-12 21:16:30 -0500

    Update to new paradigm

--- default.nix
+++ default.nix
@@ -1,20 +1,22 @@
 { nixpkgs }: let pkgs = import nixpkgs {}; in {
-  master = pkgs.runCommand "spec.json" {} ''
+  jobsets = pkgs.runCommand "spec.json" {} ''
     cat > $out <<EOF
     {
-        "enabled": 1,
-        "hidden": false,
-        "description": "js",
-        "nixexprinput": "src",
-        "nixexprpath": "release.nix",
-        "checkinterval": 300,
-        "schedulingshares": 100,
-        "enableemail": false,
-        "keepnr": 3,
-        "inputs": {
-            "src": { "type": "git", "value": "git://github.com/shlevy/declarative-hydra-example.git", "emailresponsible": false },
-            "nixpkgs": { "type": "git", "value": "git://github.com/NixOS/nixpkgs.git release-16.03", "emailresponsible": false }
-        }
+        "master" : {
+            "enabled": 1,
+            "hidden": false,
+            "description": "js",
+            "nixexprinput": "src",
+            "nixexprpath": "release.nix",
+            "checkinterval": 300,
+            "schedulingshares": 100,
+            "enableemail": false,
+            "keepnr": 3,
+            "inputs": {
+                "src": { "type": "git", "value": "git://github.com/shlevy/declarative-hydra-example.git", "emailresponsible": false },
+                "nixpkgs": { "type": "git", "value": "git://github.com/NixOS/nixpkgs.git release-16.03", "emailresponsible": false }
+            }
+	}
     }
 
     EOF

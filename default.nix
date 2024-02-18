964b19dfe932ea0b39ada0dbca1d07d6855b2cb0
wucke13 <wucke13@gmail.com>
2024-02-18 01:12:25 +0100

    fix: repair default.nix jobset output

--- default.nix
+++ default.nix
@@ -1,29 +1,46 @@
-{ nixpkgs, declInput }: let pkgs = import nixpkgs {}; in {
-  jobsets = pkgs.runCommand "spec.json" {} ''
-    cat <<EOF
-    ${builtins.toXML declInput}
-    EOF
-    cat > $out <<EOF
-    {
-        "master": {
-            "enabled": 1,
-            "hidden": false,
-            "description": "js",
-            "nixexprinput": "src",
-            "nixexprpath": "release.nix",
-            "checkinterval": 300,
-            "schedulingshares": 100,
-            "enableemail": false,
-            "emailoverride": "",
-            "keepnr": 3,
-            "inputs": {
-                "src": { "type": "git", "value": "git://github.com/shlevy/declarative-hydra-example.git", "emailresponsible": false },
-                "nixpkgs": { "type": "git", "value": "git://github.com/NixOS/nixpkgs.git release-16.03", "emailresponsible": false }
-            }
-        }
-    }
+{ nixpkgs, declInput }:
+let
+  pkgs = import nixpkgs { };
 
-    EOF
-  '';
+  defaultFlakeJobset = {
+    "enabled" = 1;
+    "hidden" = false;
+    "type" = 1; # make this a flake based jobset
+    "description" = "A meaningful jobset description";
+    "flake" = "nixpkgs#hello";
+    "checkinterval" = 300;
+    "schedulingshares" = 100;
+    "enableemail" = false;
+    # TODO find out why this must not be set
+    #"enable_dynamic_run_command" = false;
+    "emailoverride" = "";
+    "keepnr" = 5;
+  };
+
+  flakes = [
+    "github:dlr-ft/a653rs"
+    "github:dlr-ft/a653rs-linux"
+    "github:dlr-ft/a653rs-postcard"
+    "github:dlr-ft/a653rs-xng"
+    "github:dlr-ft/seL4-nix-utils"
+    "github:dlr-ft/sysml-v2-nix"
+
+    "github:aeronautical-informatics/apex-rs-vanilla"
+    "github:aeronautical-informatics/compcert-flake-utils"
+    "github:aeronautical-informatics/openCAS"
+    "github:aeronautical-informatics/openTAWS"
+    "github:aeronautical-informatics/qsma-parallel"
+    "github:aeronautical-informatics/xng-flake-utils"
+    "github:aeronautical-informatics/xng-rs"
+  ];
+
+  jobs = builtins.listToAttrs (builtins.map
+    (flakeUrl: {
+      name = pkgs.lib.strings.replaceStrings [ "/" ] [ "|" ] flakeUrl;
+      value = defaultFlakeJobset // { flake = flakeUrl; };
+    })
+    flakes);
+in
+{
+  jobsets = pkgs.writeText "generated-jobset" (builtins.toJSON jobs);
 }
-
